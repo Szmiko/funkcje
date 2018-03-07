c80a49999170203d8e90b11e9e5cf5cef0e2e703
Szymon Grabowski <szmiko@wp.pl>
2018-03-07 19:02:21 +0100

    Add function

---
+++ scripts.js
@@ -0,0 +1,20 @@
+function getTriangleArea (a, h) {
+	if (a < 0) {
+		console.log('Nieprawidłowa wartość')
+	}
+	if (h < 0) {
+		console.log('Nieprawidłowa wartość')
+	}
+	
+	return a * h / 2;
+}
+
+console.log(getTriangleArea(10, 6))
+
+var triangle1Area = getTriangleArea(8, 12);
+var triangle2Area = getTriangleArea(2,7);
+var triangle3Area = getTriangleArea(12, 19);
+
+console.log(triangle1Area)
+console.log(triangle2Area)
+console.log(triangle3Area)
